f98e8904588e916ac02113880ddd98d201c0bc36
Raquib <r.raquib01@gmail.com>
2022-10-25 17:43:21 -0600

    Re Committing for Github Actions with switched assertEquals

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -20,6 +20,6 @@ public class CustomListTest {
     public void addCityTest() {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
-        assertEquals(list.getCount(), listSize+1);
+        assertEquals(listSize+1, list.getCount());
     }
 }
